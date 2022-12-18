86032f9539ff3183f1a1f195a560bf4ef5dbe915
Minal Cheema <minaldeepcheema@gmail.com>
2022-12-18 16:48:07 +0530

    Create the interface for Welcome Page and Main menu page

--- src/LockedMe/Phase1/LockedMe_Main.java
+++ src/LockedMe/Phase1/LockedMe_Main.java
@@ -1,10 +1,12 @@
 package LockedMe.Phase1;
 
+import java.io.IOException;
+
 public class LockedMe_Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		WelcomeScreen.DisplayWelcome();
+		WelcomeScreen.displayWelcome();
 	}
 }
--- src/LockedMe/Phase1/Menu_Main_Screen.java
+++ src/LockedMe/Phase1/Menu_Main_Screen.java
@@ -1,27 +1,26 @@
 package LockedMe.Phase1;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Menu_Main_Screen {
 
 	public static void chooseOption() throws IOException {
-			System.out.println("WELCOME to LockedMe.com: Lockers Pvt. Ltd.");
-			System.out.println("==========================================");
-			System.out.println("Developed by : Minal Cheema");
-			
-			System.out.println("Please follow the instructions provided below to use the application.");
 
-			System.out.println("1.	List all the files in a specified directory.");
-			System.out.println("2.	Add a new file.");
-			System.out.println("3.	Remove an existing file.");
-			System.out.println("4.	Search a specific file.");
-			System.out.println("9.	Close the application.");
-			
-			Scanner sc = new Scanner(System.in);
-			
+		System.out.println("Please follow the instructions provided below to use the application.");
+
+		System.out.println("1.	List all the files in a specified directory.");
+		System.out.println("2.	Add a new file.");
+		System.out.println("3.	Remove an existing file.");
+		System.out.println("4.	Search a specific file.");
+		System.out.println("8.	Go back to Welcome Page.");
+		System.out.println("9.	Close the application.");
+
+		try (Scanner sc = new Scanner(System.in)) {
 			System.out.print("Please enter the number provided for the function you want to perform: ");
 			int option = sc.nextInt();
-			
+
 			switch (option) {
 			case 1: {
 				Get_FileNames listEntries = new Get_FileNames();
@@ -43,11 +42,20 @@ public class Menu_Main_Screen {
 				searchFile.searchFile();
 				break;
 			}
+			case 8: {
+				WelcomeScreen.displayWelcome();
+				break;
+			}
 			case 9: {
 				System.exit(1);
 			}
 			default:
-				throw new IllegalArgumentException("Unexpected value: " + option + ". Please select a number from given options");
+				System.out.println("Invalid input. Please select a number related to given options.\n\n");
+				chooseOption();
 			}
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input. Please select a number related to given options.\n\n");
+			chooseOption();
 		}
 	}
+}
